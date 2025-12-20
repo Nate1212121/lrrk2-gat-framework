a4c90b0d880f6cf5bbc82376ae9447eb38ae58f6
Nathan Lee <nathanl4864@gmail.com>
2025-12-19 22:47:31 -0800

    added second filter to filter_links.py that narrows nodes so they are related to parkinsons disease

--- python_steps/filter_links_1.py
+++ python_steps/filter_links_1.py
@@ -1,30 +1,62 @@
 import os
 import pandas as pd
 
-#file paths
+print("Step 1.1: Initializing")
+
+#checking directory
+print("CWD:",os.getcwd())
+print("__file__:",__file__)
+
+#FILE DIRECTORIES
 data_dir='./data/'
-new_data_dir='../new_data/'
+new_data_dir='./new_data/'
+#directories MUST be correct for the files to be found
+#(before this was ../data/ and ../new_data/ so the directories were more like /Users/nathanlee/data,
+#which is not a correct directory. instead it should be /Users/nathanlee/lrrk2gcn/data/, which would be good with ./data/)
 
 os.makedirs(data_dir,exist_ok=True)
 os.makedirs(new_data_dir,exist_ok=True)
+#just checking the directories are there and also add more robustness
 
-string_file=os.path.join(data_dir,'9606.protein.links.v12.0.txt')
+#DATA FILES
+string_link_file=os.path.join(data_dir,'9606.protein.links.v12.0.txt')
+string_info_file=os.path.join(data_dir,'9606.protein.info.v12.0.txt') #translates kegg's gene names to string's homo sapien
+#protein ids (9606.ENSP...) in this script as it contains both of those in its columns. the bridge for file intersection.
+kegg_file=os.path.join(data_dir,'kegg_pd_protein_list.txt') #the output can only contain proteins that have at least one of
+#the proteins in this file. this kegg list is made from the kegg parkinsons disease pathway proteins. this intersection-like
+#filtering will be this code's second filter
+
 score=700 #threshold for combined_score for the links (combined scores found in STRING links file)
 
-#steps start here:
-print("Step 1.1: Reading STRING links file")
+print("\nStep 1.2: Making datatables for files")
 
-string_df=pd.read_csv(string_file,sep=' ') #string dataframe from file
+string_link_df=pd.read_csv(string_link_file,sep=r'\s+') #string link dataframe from file with n spaces as separation
+string_info_df=pd.read_csv(string_info_file,sep='\t') # STRING info file dataframe
+kegg_df=pd.read_csv(kegg_file,header=None,names=['gene_name']) #kegg protein list dataframe from file
 
-print(len(string_df),"links from STRING")
+print(len(string_link_df),"interactions from STRING links")
+print(len(string_info_df),"proteins from STRING info")
+print(len(kegg_df),"proteins from KEGG Parkinson's Disease protein list")
 
-print("Step 1.2: Filtering links. Score threshold to be above:",score)
+print("\nStep 1.3: Converting KEGG gene names to STRING protein ids")
 
-filtered_df=string_df[string_df['combined_score']>score] #filtered dataframe
+parkd_protein_ids_df=pd.merge(kegg_df,string_info_df,left_on='gene_name',right_on='preferred_name')
+parkd_protein_ids=parkd_protein_ids_df['#string_protein_id'].unique().tolist()
 
-print("Filtered links count:",len(filtered_df))
+print("KEGG protein list converted to STRING protein IDs. Protein list size:",len(parkd_protein_ids))
 
-print("Step 1.3: Making edge_list.csv")
+print("\nStep 1.4: Filtering links by combined_score. combined_score must be above:",score,"for the link to be kept.")
+
+filtered_df=string_link_df[string_link_df['combined_score']>score] #filtered dataframe
+
+print("Filtered links count after combined score filtering:",len(filtered_df))
+
+print("\nStep 1.5: Filtering links by KEGG PD protein list")
+
+filtered_df=filtered_df[(filtered_df['protein1'].isin(parkd_protein_ids)) | (filtered_df['protein2'].isin(parkd_protein_ids))]
+print("Filtered links count after KEGG PD protein list filtering:",len(filtered_df))
+
+print("\nStep 1.6: Making edge_list.csv")
 
 edge_output_file=os.path.join(new_data_dir,'edge_list.csv')
 edge_df=filtered_df.copy()
@@ -32,7 +64,7 @@ edge_df.to_csv(edge_output_file,index=False)
 
 print("Edge list:",edge_output_file)
 
-print("Step 1.4: Making protein_ids.txt")
+print("\nStep 1.7: Making protein_ids.txt")
 
 unique_proteins_output_file=os.path.join(new_data_dir,'protein_ids.txt')
 unique_proteins=pd.concat([edge_df['protein1'],edge_df['protein2']]).unique()
