4f1c53500b28a7f380afe46e46673e9de283d2ce
Nathan Lee <nathanl4864@gmail.com>
2026-02-19 04:32:51 -0800

    tested v5 model, which uses mlp-like decoder and no dot product. failed because of the mismatch between linear subtraction for final_score calculation and nonlinear mlp decoding. too arbitrary also

---
+++ python_steps/7_v5_predict_links.py
@@ -0,0 +1,149 @@
+import os
+import pandas as pd
+import torch
+import torch.nn.functional as F
+from torch_geometric.data import Data
+from torch_geometric.nn import GCNConv
+from torch_geometric.transforms import RandomLinkSplit
+import numpy as np
+from sklearn.metrics import roc_auc_score
+from tqdm import tqdm
+from torch_geometric.nn import GATv2Conv
+from scipy import stats
+from scipy.stats import wilcoxon
+
+print("Step 7: Running GAT and finding differences in normal vs mutant LRRK2")
+
+print("\nStep 7.1: Initializing")
+
+#checking terminal directory
+print("CWD:",os.getcwd())
+print("__file__:",__file__)
+
+#directories
+new_data_dir='./new_data/'
+models_dir='./models/'
+results_dir='./results/'
+
+#files
+graph_file=os.path.join(new_data_dir, 'ppi_pd_graph.pt')
+gcn_v1_file=os.path.join(models_dir, 'gcn_v1.pt')
+gcn_v2_file=os.path.join(models_dir, 'gcn_v2.pt')
+gcn_v3_file=os.path.join(models_dir, 'gcn_v3.pt')
+gat_v4_file=os.path.join(models_dir, 'gat_v4.pt')
+gat_v5_file=os.path.join(models_dir, 'gat_v5.pt')
+mutant_embedding_file=os.path.join(new_data_dir, 'g2019s_protein_embedding.pt')
+results_output_file=os.path.join(results_dir, 'v5_link_prediction_results.csv')
+
+hidden_channels_one=128
+# hidden_channels_one=256
+# hidden_channels_one=512
+
+#out_channels=32
+out_channels=64
+#out_channels=128
+#out_channels=512
+
+heads=8
+
+
+class link_predictor(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels_one, out_channels, heads):
+        super().__init__()
+        self.gat1 = GATv2Conv(in_channels, hidden_channels_one, heads=heads)
+        self.mlp1 = torch.nn.Linear(hidden_channels_one*heads, hidden_channels_one)
+        self.mlp2 = torch.nn.Linear(hidden_channels_one, out_channels)
+        self.decoder_lin1 = torch.nn.Linear(out_channels*2, out_channels)
+        self.decoder_lin2 = torch.nn.Linear(out_channels, 1)
+        self.decoder_drop = torch.nn.Dropout(p=0.3)
+
+    def encode(self, x, edge_index):
+        x = self.gat1(x, edge_index).relu()
+        x = self.mlp1(x).relu()
+        x = self.mlp2(x)
+        return x
+
+    def decode(self, z, edge_label_index):
+        src = z[edge_label_index[0]]
+        dst = z[edge_label_index[1]]
+        fwd = self.decoder_lin2(
+            self.decoder_drop(
+                self.decoder_lin1(torch.cat([src, dst], dim=1)).relu()
+            )
+        ).squeeze()
+        bwd = self.decoder_lin2(
+            self.decoder_drop(
+                self.decoder_lin1(torch.cat([dst, src], dim=1)).relu()
+            )
+        ).squeeze()
+        return (fwd + bwd) / 2
+    
+if __name__=="__main__":
+    if torch.backends.mps.is_available():
+        device=torch.device('mps')
+    else:
+        device=torch.device('cpu')
+    
+    graph_data=torch.load(graph_file,weights_only=False).to(device)
+
+    mutant_embedding=torch.load(mutant_embedding_file,map_location=device)
+
+    model=link_predictor(
+        in_channels=graph_data.num_features,
+        hidden_channels_one=hidden_channels_one,
+        out_channels=out_channels,
+        heads=heads
+    ).to(device)
+
+    model.load_state_dict(torch.load(gat_v5_file,map_location=device))
+    model.eval()
+
+    lrrk2_idx=graph_data.protein_to_index['9606.ENSP00000298910']
+
+    target_row=torch.arange(graph_data.num_nodes,dtype=torch.long)
+    mask=target_row!=lrrk2_idx
+    target_row=target_row[mask]
+    source_row=torch.full((len(target_row),),fill_value=lrrk2_idx,dtype=torch.long)
+    prediction_edges=torch.stack([source_row,target_row],dim=0)
+
+    print("\nStep 7.2: Calculating scores for normal LRRK2")
+
+    encoded_normal=model.encode(graph_data.x,graph_data.edge_index)
+    decoded_normal=model.decode(encoded_normal,prediction_edges)
+
+    print("\nStep 7.3: Cloning graph for G2019S LRRK2 and calculating scores")
+    #MUTATED GRAPH MAKING
+    mutant_graph_data=graph_data.clone()
+    mutant_graph_data.x[lrrk2_idx]=mutant_embedding #replacing normal lrrk2 with g2019s lrrk2 embedding
+
+    encoded_mutant=model.encode(mutant_graph_data.x,mutant_graph_data.edge_index)
+    decoded_mutant=model.decode(encoded_mutant,prediction_edges)
+
+    score=(decoded_mutant-decoded_normal)
+
+    print("\nStep 7.4: Saving results")
+
+    id_dict={}
+    for name, i in graph_data.protein_to_index.items():
+        id_dict[i]=name
+
+    protein_names=[]
+    for i in range(graph_data.num_nodes):
+        if i!=lrrk2_idx:
+            protein_names.append(id_dict[i])
+    
+    results_df=pd.DataFrame()
+    results_df['protein_id']=protein_names
+    results_df['normal_score']=decoded_normal.detach().cpu().numpy()#to disconnect it from the neural network graph weights we have detach
+    results_df['mutant_score']=decoded_mutant.detach().cpu().numpy()#to disconnect it from the neural network graph weights we have detach
+    results_df['final_score']=score.detach().cpu().numpy()#to disconnect it from the neural network graph weights we have detach
+
+    results_df=results_df.sort_values(by='final_score',ascending=False)
+    results_df.to_csv(results_output_file,index=False)
+
+
+    gof_scores=results_df[results_df['final_score']>0]['final_score']
+    lof_scores=results_df[results_df['final_score']<0]['final_score']
+
+    statistic, p_value=wilcoxon(results_df['final_score'])
+    print(f"\nWilcoxon signed-rank test on final scores: statistic={statistic}, p-value={p_value}")
